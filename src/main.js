0f1a12ac256ddb193c2cdfdeddbfe545e0a84a6a
mohamadghayoumi <mohamad.aftabi75@gmail.com>
2024-03-15 18:32:28 +0330

    get all elements

---
+++ src/main.js
@@ -0,0 +1,43 @@
+
+const container = document.getElementById("container");
+const icon_add = document.getElementById("icon-add");
+const tbody = document.getElementById("table-tbody");
+
+
+//modalAdd
+const modal_add = document.getElementById("modal-add");
+const inputTaske = document.getElementById("inputTaske");
+const inputDeadline = document.getElementById("inputDeadline");
+const inputDescription = document.getElementById("inputDescription");
+const selectPriority = document.getElementById("selectPriority");
+const selectStatus = document.getElementById("selectStatus");
+const btnSubmit = document.getElementById("btnSubmit");
+
+//spanEror
+const errorTaske = document.getElementById("errorTaske");
+const errorPriority = document.getElementById("errorPriority");
+const errorStatus = document.getElementById("errorStatus");
+const errorDeadline = document.getElementById("errorDeadline");
+const errorDescription = document.getElementById("errorDescription");
+
+//modalEyes
+const modal_eyes = document.getElementById("modal-eyes");
+const spanModalTaske = document.getElementById("spanModalTaske");
+const spanModalStatus = document.getElementById("spanModalStatus");
+const spanModalPriority = document.getElementById("spanModalPriority");
+const spanModalDeadline = document.getElementById("spanModalDeadline");
+const spanModalDescription = document.getElementById(
+  "spanModalDescription"
+);
+const btnClose = document.getElementById("btnClose");
+
+
+//modalEdit
+const modal_edit = document.getElementById("modal-edit");
+const inputModalTaske = document.getElementById("inputModalTaske")
+const inputModalDeadline = document.getElementById("inputModalDeadline")
+const inputModalDescription = document.getElementById("inputModalDescription")
+const selectModalPriority = document.getElementById("selectModalPriority")
+const selectModalStatus = document.getElementById("selectModalStatus")
+const btnSave = document.getElementById("btnSave")
+
